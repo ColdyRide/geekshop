b70927631d58f966d8cde2f5b937fc6bc4612cba
ColdyRide <coldyride@gmail.com>
2021-07-16 20:31:27 +0300

    Optimisation of SQL queries

--- geekshop/mainapp/context_processors.py
+++ geekshop/mainapp/context_processors.py
@@ -1,11 +1,10 @@
-from basketapp.models import Basket
 from mainapp.models import ProductCategories
 
 
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = request.user.basket.select_related()
+        basket_list = request.user.basket.select_related('product')
     return {
         'basket': basket_list
     }
