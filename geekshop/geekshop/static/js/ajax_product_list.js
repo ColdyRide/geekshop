c37c0d4d99eb34008f9618472ec5867a8b99991c
ColdyRide <coldyride@gmail.com>
2021-07-18 20:40:30 +0300

    js fix

--- geekshop/geekshop/static/js/ajax_product_list.js
+++ geekshop/geekshop/static/js/ajax_product_list.js
@@ -1,6 +1,6 @@
 $( document ).on( 'click', '.details a', function(event) {
    if (event.target.hasAttribute('href')) {
-            console.log(this.target.href)
+            console.log(event.target.href)
            $.ajax({
                success: function (data) {
                    $('.products_list').html(data.result);
