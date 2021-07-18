dce0f9249b9cdb4ada054a05f41663e609a1983b
ColdyRide <coldyride@gmail.com>
2021-07-18 23:25:32 +0300

    Test fix urls

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
    path('', ProductsView.as_view(), name='category'),
    path('category/<int:pk>', ProductsView.as_view(), name='category'),
    path('product/<int:pk>', detail, name='details'),
-   path('category/<int:pk>/ajax', cache_page(3600)(ProductsView.as_view()), name='category'),
+   path('category/<int:pk>/ajax', cache_page(3600)(ProductsView.as_view())),
 ]
