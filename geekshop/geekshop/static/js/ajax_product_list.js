db8e29cdb8863fa5b5cda76b844142f88256f896
ColdyRide <coldyride@gmail.com>
2021-07-18 23:20:00 +0300

    Test fix js

--- geekshop/geekshop/static/js/ajax_product_list.js
+++ geekshop/geekshop/static/js/ajax_product_list.js
@@ -1,7 +1,7 @@
 $( document ).on( 'click', '.details a', function(event) {
    if (event.target.hasAttribute('href')) {
            $.ajax({
-               url: event.target.href+"/ajax/",
+               url: event.target.href+"/ajax",
                success: function (data) {
                    $('.details').html(data.result);
                },
