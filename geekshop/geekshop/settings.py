0aa347c9146c83a44dd928d468a0e9a9373e2e49
ColdyRide <coldyride@gmail.com>
2021-07-20 12:55:25 +0300

    fix settings.py

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -144,10 +144,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-#
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "geekshop", "static"),
-# )
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "geekshop", "static"),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
